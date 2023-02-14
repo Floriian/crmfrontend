fc0affe0bcb8dfd11f2f3476d9551808a2b76e30
Floriian <lovasifloriian@outlook.hu>
2023-02-15 00:36:45 +0100

    Create Button component

---
+++ src/components/Form/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import classNames from 'classnames';
+type Props = {
+  variant: 'danger' | 'warning' | 'confirm' | 'base';
+  children: React.ReactNode;
+  disabled?: boolean;
+  className?: string;
+  onClick?: () => void;
+};
+
+export function Button({
+  variant,
+  children,
+  disabled,
+  className,
+  onClick,
+}: Props) {
+  const buttonClass = classNames({
+    'bg-red-500 hover:bg-red-700': variant === 'danger',
+    'bg-orange-500 hover:bg-orange-800': variant === 'warning',
+    'bg-green-500 hover:bg-green-500': variant === 'confirm',
+    'bg-cyan-500 hover:bg-cyan-500': variant === 'base',
+    'bg-gray-500': disabled,
+  });
+  return (
+    <button
+      onClick={onClick}
+      className={buttonClass + ' rounded-md p-2 ' + className}
+      disabled={disabled ? true : false}
+    >
+      {children}
+    </button>
+  );
+}
