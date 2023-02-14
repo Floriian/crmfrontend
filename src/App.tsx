d55dead0d20eb4c61d6b6c129da5c07fcfd33223
Floriian <lovasifloriian@outlook.hu>
2023-02-14 21:40:24 +0100

    test fetch and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useAxios } from './hooks';
+import { routes } from './pages/routes';
 function App() {
   const { loading, error, response } = useAxios<{ name: string }>(
     'http://localhost:3000',
   );
   useEffect(() => console.log(response?.name), [response]);
-  return <div className="text-red-500">{response?.name}</div>;
+  return (
+    <div className="text-red-500">
+      asdgeci
+      {routes.map((r, i) => (
+        <h1 key={i}>{r.title}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default App;
