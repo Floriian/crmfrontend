69411696be7f7d82ee0b0299b21bcf10b8fd77e0
Floriian <lovasifloriian@outlook.hu>
2023-02-20 20:46:30 +0100

    add auth to layout.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { Menu } from './Menu';
 import Sidebar from './Sidebar';
 
 function Layout() {
+  const token = localStorage.getItem('access_token');
+  const errMessage = 'You are not logged in!';
   return (
     <>
-      {/* <Menu /> */}
-      <Sidebar>
-        <Outlet />
-      </Sidebar>
+      {token ? (
+        <>
+          {/* <Menu /> */}
+          <Sidebar>
+            <Outlet />
+          </Sidebar>
+        </>
+      ) : (
+        <Navigate
+          to={`/auth/sign-in?statusCode=401&message=${errMessage}&error=Unathorized`}
+        />
+      )}
     </>
   );
 }
