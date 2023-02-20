94aef5865de3cc65353978f3c9689aaacaca178f
Floriian <lovasifloriian@outlook.hu>
2023-02-20 17:20:42 +0100

    create axiosinstance.

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,18 +1,29 @@
-import axios from 'axios';
-import { TSignUp, TSignIn } from '../types';
+import { AxiosError } from 'axios';
+import { TSignUp, TSignIn, NestError, TNestError } from '../types';
+import { axiosInstance } from './axiosInstance';
 
 export async function signUp(dto: TSignUp) {
-  const res = await axios.post<{ access_token: string }>(
-    'http://localhost:3000/auth/sign-up',
-    { dto },
+  const res = await axiosInstance.post<{ access_token: string }>(
+    '/auth/sign-up',
+    {
+      dto,
+    },
   );
   return res.data;
 }
 
 export async function signIn(dto: TSignIn) {
-  const res = await axios.post<{ access_token: string }>(
-    'http//localhost:3000/auth/sign-in',
-    { dto },
-  );
-  return res.data;
+  try {
+    const response = await axiosInstance.post<{ access_token: string }>(
+      '/auth/sign-in',
+      dto,
+    );
+    return response;
+  } catch (e) {
+    if (e instanceof AxiosError<TNestError>) {
+      return e.response?.data;
+    }
+    console.log('is this throw...');
+    throw e;
+  }
 }
