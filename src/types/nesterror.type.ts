34c27145a353305389ef1a535de0245732a81aa7
Floriian <lovasifloriian@outlook.hu>
2023-02-20 20:47:03 +0100

    nesterror type fix.

--- src/types/nesterror.type.ts
+++ src/types/nesterror.type.ts
@@ -1,9 +1,9 @@
 import { AxiosError } from 'axios';
 
 export type TNestError = {
-  statusCode: number;
-  message?: Array<string>;
-  error: string;
+  statusCode: number | null;
+  message?: Array<string> | string | null;
+  error: string | null;
 };
 
 export interface NestError extends AxiosError<TNestError> {}
