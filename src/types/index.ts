7a4afaec73225d8aed76e5e19234c0ba902639f3
Floriian <lovasifloriian@outlook.hu>
2023-02-24 14:55:44 +0100

    user type.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './auth.type';
 export * from './nesterror.type';
 export * from './router.type';
+export * from './user.type';
---
+++ src/types/user.type.ts
@@ -0,0 +1,6 @@
+export type TUser = {
+  _id: string;
+  username: string;
+  email: string;
+  __v: number;
+};
