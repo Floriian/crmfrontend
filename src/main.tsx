0cb2c6d624b7d61058a99cb46b93f3be5a65550c
Floriian <lovasifloriian@outlook.hu>
2023-02-25 12:52:44 +0100

    initialize react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,16 @@ import { RouterProvider } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { browserRotuer } from './pages/routes';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={browserRotuer} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={browserRotuer} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
