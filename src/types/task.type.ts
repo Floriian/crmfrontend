f8175a0061b979c8cc2429b8b0678a1ff9569a42
Floriian <lovasifloriian@outlook.hu>
2023-02-25 12:54:25 +0100

    completed fields to task type

--- src/types/task.type.ts
+++ src/types/task.type.ts
@@ -1,12 +1,16 @@
 import { z } from 'zod';
 
 export const TaskSchema = z.object({
+  _id: z.string(),
   title: z.string().min(1, {
     message: 'Title is required.',
   }),
   description: z.string().min(1, {
     message: 'Description is required.',
   }),
+  completed: z.boolean().default(false),
+  completedAt: z.date(),
+  createdAt: z.date(),
 });
 
 export type TTask = z.infer<typeof TaskSchema>;
