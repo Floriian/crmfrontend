d87f7ca1aedc706008fbff4b12f3cc7238ed087e
Floriian <lovasifloriian@outlook.hu>
2023-02-25 10:11:46 +0100

    use authinstance instead of axiosInstance

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,16 +1,13 @@
 import { TUser } from '../types';
 import { getAccessToken } from '../utils';
+import { authInstance } from './authInstance';
 import { axiosInstance } from './axiosInstance';
 
 const token = getAccessToken();
 
 export const userService = {
   getUser: async function () {
-    const { data } = await axiosInstance<TUser>('/user', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const { data } = await authInstance<TUser>('/user');
     return data;
   },
 };
